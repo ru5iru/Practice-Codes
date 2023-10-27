d68d25ca7929cb421e35cff1a56e0941c7791a74
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-10-27 12:43:56 +0530

    oop 01

---
+++ JAVA/OOP/Encapsulation/MyClass.java
@@ -0,0 +1,9 @@
+package OOP.Encapsulation;
+
+public class MyClass {
+   public static void main(String[] args){
+      Person myPerson = new Person();
+      myPerson.setName("John");
+      System.out.println(myPerson.getName());
+   }
+}
---
+++ JAVA/OOP/Encapsulation/Person.java
@@ -0,0 +1,13 @@
+package OOP.Encapsulation;
+
+public class Person {
+   private String name;
+
+   public String getName(){
+      return name;
+   }
+
+   public void setName(String newName){
+      this.name = newName;
+   }
+}
