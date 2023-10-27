b8b3f805c091cd3005cbf2b007ae840c05b9fd9c
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-10-27 12:53:30 +0530

    OOP 02

---
+++ JAVA/OOP/Interfaces/MyClass.java
@@ -0,0 +1,25 @@
+package OOP.Interfaces;
+
+interface Animal {
+   public void animalSound();
+
+   public void sleep();
+}
+
+class Pig implements Animal {
+   public void animalSound() {
+      System.out.println("The pig says: wee wee");
+   }
+
+   public void sleep() {
+      System.out.println("Zzz");
+   }
+}
+
+public class MyClass {
+   public static void main(String[] args) {
+      Pig myPig = new Pig();
+      myPig.animalSound();
+      myPig.sleep();
+   }
+}
